9e5c2a7a8d610bd47015fa0b73732159ad1f41e1
Antonio Jose Barrios Leon <ajosebarrios@gmail.com>
2025-06-10 20:03:57 +0200

    fix: remove pdf routes

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,13 +2,11 @@ import { Router } from 'express';
 import authRoutes from './routes/auth.routes'; // Asegurar que exporta por defecto
 import patientRoutes from './routes/patient.routes'; // Asegurar que exporta por defecto
 import dietRoutes from './routes/diet.routes'; // Asegurar que exporta por defecto
-import pdfRoutes from './routes/pdf.routes'; // Rutas para generación de PDFs
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/patients', patientRoutes);
 router.use('/diets', dietRoutes);
-router.use('/', pdfRoutes); // Rutas PDF en la raíz para /patients/:id/combined-pdf
 
 export default router;
